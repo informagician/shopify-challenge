7e3528df75586ad4e371c6b1b870855a2da01f9e
Milo Rastgoo <msrastgoo@gmail.com>
2020-09-01 14:13:22 -0400

    initial commit - react installed

---
+++ shoppies/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>The Shoppies</h1>
+    </>
+  );
+}
+
+export default App;
